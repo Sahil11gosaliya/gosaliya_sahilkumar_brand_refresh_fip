f74c368f7184cea28a3984c12a84c12d9d45efb3
Sahil11gosaliya <s_gosaliya@fanshaweonline.ca>
2023-06-15 15:33:11 -0400

    Changes made
    Updated files, with read me, images and styling for pages. Yet more functionality remains as  more js is required along with accurate information and posters with proper gradients.

--- js/product.js
+++ js/product.js
@@ -7,3 +7,18 @@
 //(() => {  })();   
 
 // Info about IIFE https://flaviocopes.com/javascript-iife/
+
+
+
+// JavaScript code to handle carousel functionality
+const carouselImages = document.querySelectorAll('.hero-carousel img');
+let currentImageIndex = 0;
+
+function showNextImage() {
+  carouselImages[currentImageIndex].style.display = 'none';
+  currentImageIndex = (currentImageIndex + 1) % carouselImages.length;
+  carouselImages[currentImageIndex].style.display = 'block';
+}
+
+// Change the image every 3 seconds
+setInterval(showNextImage, 3000);
