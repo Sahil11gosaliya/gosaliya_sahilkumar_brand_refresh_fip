995260b7b427414c4860d169d72cd7ab2551538b
Sahil11gosaliya <s_gosaliya@fanshaweonline.ca>
2023-08-07 16:52:16 -0400

    Final changes

--- js/about.js
+++ js/about.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Get the current page URL
+
     const currentURL = window.location.href;
   
     // Get all navigation links
     const navLinks = document.querySelectorAll('.nav-button');
   
-    // Add active class to the corresponding link
+    // active class to the corresponding link
     navLinks.forEach(link => {
       if (link.href === currentURL) {
         link.classList.add('active');
--- js/product.js
+++ js/product.js
@@ -20,13 +20,12 @@ document.addEventListener("DOMContentLoaded", function() {
 //active link
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Get the current page URL
+
   const currentURL = window.location.href;
 
-  // Get all navigation links
+
   const navLinks = document.querySelectorAll('.nav-button');
 
-  // Add active class to the corresponding link
   navLinks.forEach(link => {
     if (link.href === currentURL) {
       link.classList.add('active');
